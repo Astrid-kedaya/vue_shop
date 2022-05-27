e90b87091016346b5539d090e765773452e6d757
Astrid-kedaya <799041536@qq.com>
2022-05-27 16:50:31 +0800

    商品功能开发完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -93,7 +93,7 @@ export default {
       const { data: res } = await this.$http.get("menus");
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
       this.menuList = res.data;
-      console.log(res);
+      // console.log(res);
     },
     // 点击按钮，侧边栏折叠、展开
     toggleCollapse() {
--- src/main.js
+++ src/main.js
@@ -8,10 +8,18 @@ import './assets/fonts/iconfont.css'
 import "./assets/css/global.css";
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://139.9.202.95:8888/api/private/v1'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
 axios.interceptors.request.use(config => {
+  // 除了登录页面，其他都需token，这个操作实现每次请求前,自动加token
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // console.log(config);
   // 必须return
@@ -23,6 +31,24 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 日期格式化
+Vue.filter('dateFormat', function (originVal) {
+  // 获取的时间戳是秒为单位，转化时是毫秒，所以*1000
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  // 先变成字符串，然后padstart，不足2位用0填充
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + 1 + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
