bba48f43ee7fb741a86976520d11109bcf686ca5
Astrid-kedaya <799041536@qq.com>
2022-05-31 12:48:05 +0800

    进度条配置

--- src/main.js
+++ src/main.js
@@ -15,16 +15,33 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// 加载进度条的包
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
 import axios from 'axios'
+import { start } from 'nprogress'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+// 在 request 拦截器中展示进度条
 axios.interceptors.request.use(config => {
+  // 展示进度条
+  NProgress.start()
   // 除了登录页面，其他都需token，这个操作实现每次请求前,自动加token
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // console.log(config);
   // 必须return
   return config
 })
+
+// 在 response 拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  // 隐藏进度条
+  NProgress.done()
+  return config
+})
+
 // 挂载axios
 Vue.prototype.$http = axios
 
